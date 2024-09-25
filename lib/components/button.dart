2f7cddc0dafefbf7fa70499915a8c79f78963e0c
keannugran19 <main.grankeannu@gmail.com>
2024-09-25 11:43:09 +0800

    UPDATE button design

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -1,33 +1,30 @@
+import 'package:babysitterapp/styles/size.dart';
 import 'package:flutter/material.dart';
 
 class AppButton extends StatelessWidget {
-  final double width;
-  final double height;
   final Function()? onPressed;
   final String text;
 
-  const AppButton(
-      {super.key,
-      required this.width,
-      required this.text,
-      required this.height,
-      this.onPressed});
+  const AppButton({super.key, required this.text, this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     var buttonStyle = ElevatedButton.styleFrom(
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)));
     var textStyle = const TextStyle(fontWeight: FontWeight.w500);
+    var padding = const EdgeInsets.symmetric(horizontal: 15);
 
-    return SizedBox(
-      width: width,
-      height: height,
-      child: ElevatedButton(
-        onPressed: onPressed,
-        style: buttonStyle,
-        child: Text(
-          text,
-          style: textStyle,
+    return Padding(
+      padding: padding,
+      child: SizedBox(
+        width: sizeConfig.widthSize(context),
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: buttonStyle,
+          child: Text(
+            text,
+            style: textStyle,
+          ),
         ),
       ),
     );
