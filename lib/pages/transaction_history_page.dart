0db91dc1ac91a8d23adac5799e57f02c846fcb78
Wol Vic John C. Lim <wolvicjohn@gmail.com>
2024-10-24 01:51:35 +0800

    tranhispage updates

--- lib/pages/transaction_history_page.dart
+++ lib/pages/transaction_history_page.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-
 import '../transaction/transactionhistorydata.dart';
 import '../transaction/transactionhistorymodel.dart';
 import 'transactioninfopage.dart';
+import '/styles/colors.dart';
+import '/styles/theme_data.dart';
 
 class TransactionHistoryPage extends StatefulWidget {
   @override
@@ -11,7 +12,8 @@ class TransactionHistoryPage extends StatefulWidget {
 }
 
 class _TransactionHistoryPageState extends State<TransactionHistoryPage> {
-  String selectedStatus = 'All'; // Default filter option
+  String selectedStatus = 'All';
+  String currentDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +23,7 @@ class _TransactionHistoryPageState extends State<TransactionHistoryPage> {
     for (var transaction in transactions) {
       // Filter transactions based on selected status
       if (selectedStatus != 'All' && transaction.status != selectedStatus) {
-        continue; // Skip transactions that don't match the selected status
+        continue;
       }
 
       final String monthYear =
@@ -55,89 +57,149 @@ class _TransactionHistoryPageState extends State<TransactionHistoryPage> {
             const Text('Transaction History'),
           ],
         ),
-        actions: [
-          // Filter Dropdown
-          Padding(
-            padding: const EdgeInsets.only(right: 16.0),
-            child: DropdownButton<String>(
-              value: selectedStatus,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selectedStatus = newValue!;
-                });
-              },
-              items: <String>['All', 'Confirmed', 'Cancelled']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
+      ),
+      body: Column(
+        children: [
+          // Row for "Sort By" text and Dropdown Filter
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white, // Background color
+              borderRadius: BorderRadius.circular(8.0), // Rounded corners
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  spreadRadius: 2, // Shadow spread radius
+                  blurRadius: 5, // Shadow blur radius
+                  offset: const Offset(0, 3), // Shadow position
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Current Date Text
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'As of $currentDate',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: accentColor,
+                          ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+
+                  // Sort By Text
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Text(
+                      'Sort By:',
+                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: accentColor,
+                          ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+
+                  const SizedBox(width: 10),
+                  DropdownButton<String>(
+                    value: selectedStatus,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedStatus = newValue!;
+                      });
+                    },
+                    items: <String>['All', 'Confirmed', 'Cancelled']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(
+                          value,
+                          style: Theme.of(context).textTheme.bodyMedium,
+                        ),
+                      );
+                    }).toList(),
+                    underline: Container(
+                      height: 2,
+                      color: accentColor,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          Expanded(
+            child: ListView.builder(
+              itemCount: sortedMonthKeys.length,
+              itemBuilder: (context, index) {
+                final String monthYear = sortedMonthKeys[index];
+                final List<Transaction> monthTransactions =
+                    groupedTransactions[monthYear]!;
+
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(height: 15),
+                    // Month header
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        monthYear,
+                        style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                    ),
+                    ...monthTransactions.map((transaction) {
+                      return GestureDetector(
+                        onTap: () => _navigateToBabysitterDetails(
+                          context,
+                          transaction.babysitterName,
+                          transaction.transactionId,
+                          transaction.bookingDate,
+                        ),
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 5, horizontal: 8),
+                          decoration: BoxDecoration(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            borderRadius: BorderRadius.circular(8.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.2),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: const Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: ListTile(
+                            leading: statusIcon(transaction.status),
+                            title: Text(
+                              'Status: ${transaction.status}',
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .bodyMedium
+                                  ?.copyWith(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              'Date: ${DateFormat('yyyy-MM-dd').format(transaction.bookingDate)}',
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ],
                 );
-              }).toList(),
+              },
             ),
           ),
         ],
-      ),
-      body: ListView.builder(
-        itemCount: sortedMonthKeys.length,
-        itemBuilder: (context, index) {
-          final String monthYear = sortedMonthKeys[index];
-          final List<Transaction> monthTransactions =
-              groupedTransactions[monthYear]!;
-
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const SizedBox(height: 15),
-              // Month header
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'As of $monthYear',
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              ...monthTransactions.map((transaction) {
-                return GestureDetector(
-                  onTap: () => _navigateToBabysitterDetails(
-                    context,
-                    transaction.babysitterName,
-                    transaction.transactionId,
-                    transaction.bookingDate,
-                  ),
-                  child: Container(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(8.0),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.2),
-                          spreadRadius: 1,
-                          blurRadius: 5,
-                          offset: const Offset(0, 3),
-                        ),
-                      ],
-                    ),
-                    child: ListTile(
-                      leading: statusIcon(transaction.status),
-                      title: Text(
-                        'Status: ${transaction.status}',
-                        style: const TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Date: ${DateFormat('yyyy-MM-dd').format(transaction.bookingDate)}',
-                      ),
-                    ),
-                  ),
-                );
-              }).toList(),
-            ],
-          );
-        },
       ),
     );
   }
